ba316e9e9f0fb5e875e651e4c1bf67212c464948
phl1m17 <clarkmanag@gmail.com>
2026-04-01 17:39:08 -0400

    fixed mainScreen

--- src/ui/MainScreen.java
+++ src/ui/MainScreen.java
@@ -10,10 +10,13 @@ import javax.swing.JLabel;
 public class MainScreen implements ActionListener {
     Panel panel;
     boolean active = true;
-    private final Font titleFont = new Font("Arial", Font.BOLD, 40);
+
+    private final Font titleFont = new Font("Arial", Font.BOLD, 48);
+    private final Font descFont = new Font("Arial", Font.PLAIN, 15);
     private final Font defaultFont = new Font("Arial", Font.PLAIN, 12);
+
     private JButton startButton;
-    private JLabel subtitleLabel;
+    private JLabel controlLabel;
 
     public MainScreen(Panel panel) {
         this.panel = panel;
@@ -23,41 +26,59 @@ public class MainScreen implements ActionListener {
         startButton.addActionListener(this);
         panel.add(startButton);
 
-        subtitleLabel = new JLabel(
+        controlLabel = new JLabel(
                 "<html><center>" +
-                        "<br><br><b>How to Play</b><br><br>" +
-                        "Move: Left Right Arrow / A D &nbsp;&nbsp; Jump: Up Arrow / SPACE <br>" +
-                        "Inventory: E &nbsp;&nbsp; Pause: ESC<br><br>" +
-                        "Chop trees, craft weapons, survive the night!<br><br><br><br><br>" +
-                        "<i>By Phil Clarence Manag</i><br>" +
-                        "<i>Student #: 501351456</i>" +
+                        "<b>Controls</b><br><br>" +
+                        "Left: A &nbsp;&nbsp; Right: D &nbsp;&nbsp; Jump: SPACE <br>(or arrow keys)<br><br>" +
+                        "Inventory: E &nbsp;&nbsp;&nbsp; Pause: ESC<br>" +
+                        "Attack / Break: Left Click &nbsp;&nbsp; Place: Right Click<br><br><br>" +
+                        "<i>By Phil Clarence Manag &nbsp;&mdash;&nbsp; Student #: 501351456</i>" +
                         "</center></html>");
-        subtitleLabel.setForeground(Color.WHITE);
-        subtitleLabel.setHorizontalAlignment(JLabel.CENTER);
-        subtitleLabel.setBounds(panel.SCREEN_WIDTH / 2 - 200, panel.SCREEN_HEIGHT / 2 + 30, 400, 250);
-        panel.add(subtitleLabel);
+        controlLabel.setForeground(Color.WHITE);
+        controlLabel.setHorizontalAlignment(JLabel.CENTER);
+        controlLabel.setBounds(panel.SCREEN_WIDTH / 2 - 220, panel.SCREEN_HEIGHT / 2 + 65, 440, 220);
+        panel.add(controlLabel);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         active = false;
         startButton.setVisible(false);
-        subtitleLabel.setVisible(false);
+        controlLabel.setVisible(false);
     }
 
     public void paint(Graphics2D g) {
         if (!active)
             return;
 
-        g.setColor(new Color(109, 199, 112));
+        g.setColor(new Color(30, 120, 50));
         g.fillRect(0, 0, panel.SCREEN_WIDTH, panel.SCREEN_HEIGHT);
 
-        g.setColor(Color.WHITE);
+        int cx = panel.SCREEN_WIDTH / 2;
+        int titleY = panel.SCREEN_HEIGHT / 4;
+
         g.setFont(titleFont);
-        String title = "2D Game";
+        g.setColor(Color.WHITE);
         FontMetrics fm = g.getFontMetrics();
-        int x = panel.SCREEN_WIDTH / 2 - fm.stringWidth(title) / 2;
-        g.drawString(title, x, panel.SCREEN_HEIGHT / 3);
+        String title = "2D Survival Game";
+        g.drawString(title, cx - fm.stringWidth(title) / 2, titleY);
+
+        g.setColor(new Color(0, 0, 0, 60));
+        g.drawString(title, cx - fm.stringWidth(title) / 2 + 2, titleY + 2);
+        g.setColor(Color.WHITE);
+        g.drawString(title, cx - fm.stringWidth(title) / 2, titleY);
+
+        g.setFont(descFont);
+        g.setColor(new Color(220, 255, 220));
+        fm = g.getFontMetrics();
+
+        String line1 = "Chop trees, craft weapons, and build shelter before the night falls.";
+        String line2 = "How long can you survive?";
+        int descY = titleY + 50;
+
+        g.drawString(line1, cx - fm.stringWidth(line1) / 2, descY);
+        g.drawString(line2, cx - fm.stringWidth(line2) / 2, descY + fm.getHeight() + 4);
+
         g.setFont(defaultFont);
     }
 }
